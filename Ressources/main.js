9fbc6542552715306b46a7a8fc700c970d30f8bc
Yohann <73314529+yohann69@users.noreply.github.com>
2022-03-09 23:15:53 +0100

    Building the Contributions

---
+++ Ressources/main.js
@@ -0,0 +1,14 @@
+"use strict";
+/*------------------------------------------------
+					Project Name
+			  ~ Written By @Yohann69 ~
+-----------------------v0.1---------------------*/
+
+
+console.log("Welcome on Project Name");
+console.log("This project has been created by @Yohann69");
+
+
+/*------------------------------------------------------------
+					~ HamburgerButton ~
+------------------------------------------------------------*/
