1488a2c9ed949badf9fca9b6e96772dffcbec974
Yohann <73314529+yohann69@users.noreply.github.com>
2022-10-20 10:04:31 +0200

    Homepage

--- css/main.js
+++ css/main.js
@@ -14,14 +14,79 @@ console.log("This project has been created by @Yohann69");
 ------------------------------------------------------------*/
 
 
-function fillemail(){
-	
+function fillemail() {
+
 	const subject = document.querySelector('.subject').value;
 	const message = document.querySelector('.message').value;
-	const email= document.querySelector('.emailaddress');
+	const email = document.querySelector('.emailaddress');
 
-	email.href=`mailto:yohann@chavanel.eu.org?subject=${subject}&body=${message}`
+	email.href = `mailto:yohann@chavanel.eu.org?subject=${subject}&body=${message}`
 
 	email.click();
-		
+
 }
+
+
+
+
+/*------------------------------------------------------------
+					~ Animate text ~
+------------------------------------------------------------*/
+
+
+var TxtRotate = function (el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+
+	this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+	var that = this;
+	var delta = 300 - Math.random() * 100;
+
+	if (this.isDeleting) { delta /= 3; }
+
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
+	}
+
+	setTimeout(function () {
+		that.tick();
+	}, delta);
+};
+
+window.onload = function () {
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i = 0; i < elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-rotate');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+			new TxtRotate(elements[i], JSON.parse(toRotate), period);
+		}
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+	document.body.appendChild(css);
+};
